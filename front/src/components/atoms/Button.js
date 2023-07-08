24dda35ad934702400f54c158c9ca82ffcf42c80
Simin-Kim <show3391@gmail.com>
2023-07-08 16:49:56 +0900

    #7 feat: Atom 컴포넌트 구현

---
+++ front/src/components/atoms/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import button from './css/Button.module.css';
+
+const Button = ({ children, width, type, handler }) => {
+    return (
+        <div className={`${button.wrapper} ${button[`${width}`]}`}>
+            <button className={`${button.button} ${button[`${type}`]}`} onClick={handler}>
+                <span>{children}</span>
+            </button>
+        </div>
+    );
+};
+
+export default Button;
