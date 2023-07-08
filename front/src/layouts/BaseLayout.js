b4f8e662010690b38fa9d7310456aa8a96432cba
Simin-Kim <show3391@gmail.com>
2023-07-08 16:51:02 +0900

    fix: 클래스 이름 수정

--- front/src/layouts/BaseLayout.js
+++ front/src/layouts/BaseLayout.js
@@ -2,9 +2,9 @@ import Base from './BaseLayout.module.css';
 
 const BaseLayout = ({ children }) => {
     return (
-        <div className={Base.wrapper}>
-            <div className={Base.appWrapper}>
-                <div className={Base.appContainer}>{children}</div>
+        <div className={Base.container}>
+            <div className={Base.wrapper}>
+                <div className={Base.contents}>{children}</div>
             </div>
         </div>
     );
