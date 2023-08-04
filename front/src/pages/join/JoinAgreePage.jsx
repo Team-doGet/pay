1af0af19b636b1a07d80d15857b62a879df55e5c
Simin-Kim <show3391@gmail.com>
2023-08-04 16:35:39 +0900

    #54 feat: 약관동의 기능 구현

--- front/src/pages/join/JoinAgreePage.jsx
+++ front/src/pages/join/JoinAgreePage.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Agreements from '../../components/molecules/agree/Agreements';
 import BottomButtons from '../../components/molecules/common/BottomButtons';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { joinState } from '../../states/joinState';
 
 const JoinAgreePage = () => {
     const navigate = useNavigate();
+    const { agree1Yn, agree2Yn } = useRecoilValue(joinState);
+    const [warning, setWarning] = useState(false);
+
+    const nextHandler = () => {
+        if (agree1Yn && agree2Yn) {
+            navigate('/join/info');
+        } else {
+            setWarning(true);
+        }
+    };
 
     return (
         <div>
-            <Agreements />
-            <BottomButtons childrens={['다음']} handlers={[() => navigate('/join/info')]} />
+            <Agreements warning={warning} />
+            <BottomButtons childrens={['다음']} handlers={[() => nextHandler()]} />
         </div>
     );
 };
